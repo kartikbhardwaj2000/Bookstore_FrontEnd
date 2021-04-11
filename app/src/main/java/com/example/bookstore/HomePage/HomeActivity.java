bd9e7d8e8c99775ad6ecf518f129274684751e38
kartik bhardwaj <kartikbhardwaj016@gmail.com>
2021-04-11 21:07:46 +0530

    rectify import R error

--- app/src/main/java/com/example/bookstore/HomePage/HomeActivity.java
+++ app/src/main/java/com/example/bookstore/HomePage/HomeActivity.java
@@ -2,7 +2,7 @@ package com.example.bookstore.HomePage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.example.bookstore.R;
 public class HomeActivity extends AppCompatActivity {
 
     @Override
